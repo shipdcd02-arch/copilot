c9d4eb4b70226795e277fadd94d8f737623e5210
shipdcd02-arch <shi.pd.cd02@gmail.com>
2026-04-03 14:26:42 +0900

    Upload test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 (defun c:BLK_BOUND_FIX (/ ent obj insPt rot sx sy sz blkName blkDef 
-                          allMin allMax oName tmpMin tmpMax p1 p2 p3 p4)
+                          allMin allMax oName tmpMin tmpMax p1 p2 p3 p4 tr-pt)
   (vl-load-com)
 
   ;; 1. 블록 선택
@@ -7,14 +7,15 @@
   
   (if (and ent (= (cdr (assoc 0 (entget ent))) "INSERT"))
     (progn
-      (setq obj (vlax-ename->vla-object ent)
-            insPt (vlax-safearray->list (vlax-variant-value (vla-get-InsertionPoint obj)))
-            rot (vla-get-Rotation obj)
-            ;; [수정] 축척은 X, Y, Z 각각 가져와야 함
-            sx (vla-get-ScaleFactorX obj)
-            sy (vla-get-ScaleFactorY obj)
-            sz (vla-get-ScaleFactorZ obj)
-            blkName (vla-get-Name obj)
+      (setq obj (vlax-ename->vla-object ent))
+      
+      ;; [수정] 2018 버전 및 범용성을 위해 vlax-get-property 사용
+      (setq insPt (vlax-safearray->list (vlax-variant-value (vlax-get-property obj 'InsertionPoint)))
+            rot   (vlax-get-property obj 'Rotation)
+            sx    (vlax-get-property obj 'XScaleFactor)
+            sy    (vlax-get-property obj 'YScaleFactor)
+            sz    (vlax-get-property obj 'ZScaleFactor)
+            blkName (vlax-get-property obj 'Name)
             blkDef (vla-item (vla-get-blocks (vla-get-activedocument (vlax-get-acad-object))) blkName)
             allMin nil
             allMax nil)
@@ -22,7 +23,6 @@
       ;; 2. 블록 내부 객체 순회 (OLE, HATCH 제외)
       (vlax-for subObj blkDef
         (setq oName (vla-get-ObjectName subObj))
-        ;; OLE와 Hatch는 계산에서 완전히 제외
         (if (not (or (wcmatch oName "AcDbOle*") (wcmatch oName "AcDbHatch")))
           (if (not (vl-catch-all-error-p (vl-catch-all-apply 'vla-getboundingbox (list subObj 'tmpMin 'tmpMax))))
             (progn
@@ -39,27 +39,29 @@
         )
       )
 
-      ;; 3. 좌표 변환 및 출력
-      (if (and allMin allMax)
-        (let ( (tr-pt (lambda (pt)
-                        (let* ((x (* (car pt) sx))
-                               (y (* (cadr pt) sy))
-                               (nx (+ (- (* x (cos rot)) (* y (sin rot))) (car insPt)))
-                               (ny (+ (+ (* x (sin rot)) (* y (cos rot))) (cadr insPt))))
-                          (list nx ny (+ (caddr pt) (caddr insPt)))))) )
-          
-          (setq p1 (tr-pt allMin) ;; 좌하
-                p2 (tr-pt (list (car allMax) (cadr allMin) (caddr allMin))) ;; 우하
-                p3 (tr-pt allMax) ;; 우상
-                p4 (tr-pt (list (car allMin) (cadr allMax) (caddr allMin)))) ;; 좌상
+      ;; 3. 좌표 변환 함수 정의 (회전, 축척, 이동 적용)
+      (setq tr-pt (lambda (pt)
+                    (let* ((x (* (car pt) sx))
+                           (y (* (cadr pt) sy))
+                           (nx (+ (- (* x (cos rot)) (* y (sin rot))) (car insPt)))
+                           (ny (+ (+ (* x (sin rot)) (* y (cos rot))) (cadr insPt))))
+                      (list nx ny (+ (* (caddr pt) sz) (caddr insPt))))))
 
-          (princ "\n--- [추출된 실제 좌표] ---")
-          (princ (strcat "\n좌측 하단: " (vl-prin1-to-string p1)))
-          (princ (strcat "\n우측 하단: " (vl-prin1-to-string p2)))
-          (princ (strcat "\n우측 상단: " (vl-prin1-to-string p3)))
-          (princ (strcat "\n좌측 상단: " (vl-prin1-to-string p4)))
+      ;; 4. 결과 도출 및 출력
+      (if (and allMin allMax)
+        (progn
+          (setq p1 (apply tr-pt (list allMin)) ;; 좌하
+                p2 (apply tr-pt (list (list (car allMax) (cadr allMin) (caddr allMin)))) ;; 우하
+                p3 (apply tr-pt (list allMax)) ;; 우상
+                p4 (apply tr-pt (list (list (car allMin) (cadr allMax) (caddr allMin))))) ;; 좌상
+
+          (princ "\n--- [OLE/해치 제외 추출 결과] ---")
+          (princ (strcat "\n좌하단 (P1): " (vl-prin1-to-string p1)))
+          (princ (strcat "\n우하단 (P2): " (vl-prin1-to-string p2)))
+          (princ (strcat "\n우상단 (P3): " (vl-prin1-to-string p3)))
+          (princ (strcat "\n좌상단 (P4): " (vl-prin1-to-string p4)))
         )
-        (princ "\n오류: OLE/해치를 제외하면 블록 내에 계산 가능한 객체가 없습니다.")
+        (princ "\n오류: 블록 내에 계산 가능한 일반 객체(선, 원 등)가 없습니다.")
       )
     )
     (princ "\n블록(INSERT) 객체가 아닙니다.")
