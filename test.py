e53acb0c078a71bb2fd2f7677be46f8b799b8e08
shipdcd02-arch <shi.pd.cd02@gmail.com>
2026-04-03 14:29:38 +0900

    Upload test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@
     (progn
       (setq obj (vlax-ename->vla-object ent))
       
-      ;; [수정] 2018 버전 및 범용성을 위해 vlax-get-property 사용
+      ;; 블록 속성 추출 (2018 버전용 XScaleFactor 등 사용)
       (setq insPt (vlax-safearray->list (vlax-variant-value (vlax-get-property obj 'InsertionPoint)))
             rot   (vlax-get-property obj 'Rotation)
             sx    (vlax-get-property obj 'XScaleFactor)
@@ -39,29 +39,34 @@
         )
       )
 
-      ;; 3. 좌표 변환 함수 정의 (회전, 축척, 이동 적용)
-      (setq tr-pt (lambda (pt)
-                    (let* ((x (* (car pt) sx))
-                           (y (* (cadr pt) sy))
-                           (nx (+ (- (* x (cos rot)) (* y (sin rot))) (car insPt)))
-                           (ny (+ (+ (* x (sin rot)) (* y (cos rot))) (cadr insPt))))
-                      (list nx ny (+ (* (caddr pt) sz) (caddr insPt))))))
+      ;; 3. 좌표 변환 함수 (tr-pt) 정의 - 직접 호출 방식으로 변경
+      (defun my-transform (pt / x y nx ny)
+        (setq x (* (car pt) sx)
+              y (* (cadr pt) sy))
+        (setq nx (+ (- (* x (cos rot)) (* y (sin rot))) (car insPt))
+              ny (+ (+ (* x (sin rot)) (* y (cos rot))) (cadr insPt)))
+        (list nx ny (+ (* (caddr pt) sz) (caddr insPt)))
+      )
 
       ;; 4. 결과 도출 및 출력
       (if (and allMin allMax)
         (progn
-          (setq p1 (apply tr-pt (list allMin)) ;; 좌하
-                p2 (apply tr-pt (list (list (car allMax) (cadr allMin) (caddr allMin)))) ;; 우하
-                p3 (apply tr-pt (list allMax)) ;; 우상
-                p4 (apply tr-pt (list (list (car allMin) (cadr allMax) (caddr allMin))))) ;; 좌상
+          ;; 각 꼭지점 계산
+          (setq p1 (my-transform allMin) ;; 좌하
+                p2 (my-transform (list (car allMax) (cadr allMin) (caddr allMin))) ;; 우하
+                p3 (my-transform allMax) ;; 우상
+                p4 (my-transform (list (car allMin) (cadr allMax) (caddr allMin)))) ;; 좌상
 
-          (princ "\n--- [OLE/해치 제외 추출 결과] ---")
-          (princ (strcat "\n좌하단 (P1): " (vl-prin1-to-string p1)))
-          (princ (strcat "\n우하단 (P2): " (vl-prin1-to-string p2)))
-          (princ (strcat "\n우상단 (P3): " (vl-prin1-to-string p3)))
-          (princ (strcat "\n좌상단 (P4): " (vl-prin1-to-string p4)))
+          (princ "\n--- [추출 결과 (OLE/해치 제외)] ---")
+          (princ (strcat "\n1. 좌측 하단: " (vl-prin1-to-string p1)))
+          (princ (strcat "\n2. 우측 하단: " (vl-prin1-to-string p2)))
+          (princ (strcat "\n3. 우측 상단: " (vl-prin1-to-string p3)))
+          (princ (strcat "\n4. 좌측 상단: " (vl-prin1-to-string p4)))
+          
+          ;; (선택 사항) 제대로 잡혔는지 확인용 사각형 그리기
+          ;; (command "_pline" p1 p2 p3 p4 "_c")
         )
-        (princ "\n오류: 블록 내에 계산 가능한 일반 객체(선, 원 등)가 없습니다.")
+        (princ "\n오류: 계산 가능한 일반 객체가 블록 내에 없습니다.")
       )
     )
     (princ "\n블록(INSERT) 객체가 아닙니다.")
