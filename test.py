c49db18ba5b23f8890d8e826ca979ed6e9d1c66d
shipdcd02-arch <shi.pd.cd02@gmail.com>
2026-04-07 11:39:09 +0900

    Upload test.py

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-subprocess.Popen(
-    [exe_path, tmp_path],
-    stdin=subprocess.DEVNULL,       # 없는 핸들 대신 /dev/null 연결
-    stdout=subprocess.DEVNULL,
-    stderr=subprocess.DEVNULL,
-    creationflags=subprocess.CREATE_NO_WINDOW,  # 콘솔 창 없이 실행
-)
+cmd = f'"{exe_path}" "{tmp_path}"'
+subprocess.Popen(cmd, shell=True, ...)
