fea00c4452826fd8c529b3ecf7075b92c9170266
shipdcd02-arch <shi.pd.cd02@gmail.com>
2026-04-21 16:56:43 +0900

    Upload test.py

--- test.py
+++ test.py
@@ -1,18 +1,32 @@
-(defun C:MX100 (/ ss i ent obj)
+(defun C:MX100 (/ ss i obj gr key)
   (vl-load-com)
   (setq ss (ssget "_I"))
   (if (null ss) (setq ss (ssget)))
   (if ss
     (progn
-      (setq i 0)
-      (while (< i (sslength ss))
-        (setq obj (vlax-ename->vla-object (ssname ss i)))
-        (vla-Move obj
-          (vlax-3d-point '(0 0 0))
-          (vlax-3d-point '(100 0 0)))
-        (setq i (1+ i))
-      )
       (sssetfirst nil ss)
+      (princ "\n[Space/Enter: +100mm]  [ESC: 종료]")
+      (while
+        (progn
+          (setq gr (grread nil 2 0))
+          (setq key (cadr gr))
+          (cond
+            ((or (= key 32) (= key 13))
+             (setq i 0)
+             (while (< i (sslength ss))
+               (setq obj (vlax-ename->vla-object (ssname ss i)))
+               (vla-Move obj
+                 (vlax-3d-point '(0 0 0))
+                 (vlax-3d-point '(100 0 0)))
+               (setq i (1+ i))
+             )
+             (sssetfirst nil ss)
+             T)
+            ((= key 27) nil)
+            (T T)
+          )
+        )
+      )
     )
   )
   (princ)
