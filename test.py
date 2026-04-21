be56d6a8da54426db5b3e4beed1b8927bd9fd42e
shipdcd02-arch <shi.pd.cd02@gmail.com>
2026-04-21 16:53:33 +0900

    Upload test.py

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
-(defun C:MX100 ()
+(defun C:MX100 (/ ss i ent obj)
+  (vl-load-com)
   (setq ss (ssget "_I"))
-  (if (null ss)
-    (setq ss (ssget)))
+  (if (null ss) (setq ss (ssget)))
   (if ss
     (progn
-      (command "._MOVE" ss "" "0,0,0" "100,0,0")
+      (setq i 0)
+      (while (< i (sslength ss))
+        (setq obj (vlax-ename->vla-object (ssname ss i)))
+        (vla-Move obj
+          (vlax-3d-point '(0 0 0))
+          (vlax-3d-point '(100 0 0)))
+        (setq i (1+ i))
+      )
       (sssetfirst nil ss)
     )
   )
