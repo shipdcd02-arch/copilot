ce651ce4ff3b1e3acb6a97a636837108546bab8e
shipdcd02-arch <shi.pd.cd02@gmail.com>
2026-04-24 10:10:35 +0900

    Upload test.py

--- test.py
+++ test.py
@@ -1,33 +1,14 @@
-(defun C:MX100 (/ ss i obj gr key)
-  (vl-load-com)
-  (setq ss (ssget "_I"))
-  (if (null ss) (setq ss (ssget)))
+(defun c:GEN2D (/ ss)
+  (setq ss (ssget ":S" '((0 . "SECTIONPLANE")))) ; 단면 평면 객체 하나 선택
+  
   (if ss
     (progn
-      (sssetfirst nil ss)
-      (princ "\n[Space/Enter: +100mm]  [ESC: 종료]")
-      (while
-        (progn
-          (setq gr (grread nil 2 0))
-          (setq key (cadr gr))
-          (cond
-            ((or (= key 32) (= key 13))
-             (setq i 0)
-             (while (< i (sslength ss))
-               (setq obj (vlax-ename->vla-object (ssname ss i)))
-               (vla-Move obj
-                 (vlax-3d-point '(0 0 0))
-                 (vlax-3d-point '(100 0 0)))
-               (setq i (1+ i))
-             )
-             (sssetfirst nil ss)
-             T)
-            ((= key 27) nil)
-            (T T)
-          )
-        )
-      )
+      ;; 명령행 버전으로 단면 블록 생성
+      ;; 옵션 순서: 객체 선택 -> 2D -> 파일저장(N) -> 삽입점(0,0) -> 스케일(1) -> 회전(0)
+      (command "-SECTIONPLANETOBLOCK" (ssname ss 0) "2" "N" "0,0,0" "1" "1" "0")
+      (princ "\n단면이 2D 블록으로 추출되어 (0,0,0) 위치에 삽입되었습니다.")
     )
+    (princ "\n단면 평면을 찾을 수 없습니다.")
   )
   (princ)
 )
