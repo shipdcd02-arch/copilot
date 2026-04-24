a5fe97ef57f475581c94f505be18c207d00c7b03
shipdcd02-arch <shi.pd.cd02@gmail.com>
2026-04-24 10:32:41 +0900

    Upload test.py

--- test.py
+++ test.py
@@ -1,23 +1,13 @@
-(defun c:GEN2D_AUTO (/ ent obj)
+(defun c:SectionToBlockAX ( / doc sel obj)
   (vl-load-com)
-  ;; 1. SECTIONOBJECT 선택
-  (if (setq ent (car (entsel "\n단면 객체를 선택하세요: ")))
+  (setq doc (vla-get-ActiveDocument (vlax-get-acad-object)))
+  ;; 사용자에게 섹션 플레인 선택 요청
+  (setq sel (car (entsel "\n섹션 플레인 선택: ")))
+  (if sel
     (progn
-      (setq obj (vlax-ename->vla-object ent))
-      
-      ;; 2. 객체가 진짜 단면 객체인지 확인
-      (if (= (vla-get-ObjectName obj) "AcDbSection")
-        (progn
-          ;; 3. 2D 단면 블록 생성 함수 호출
-          ;; (vla-GenerateSectionBlock 객체 대상객체(보통 현재도면) 블록이름)
-          ;; 여기서는 수동으로 대화창 없이 처리하기 위해 내부 메서드를 활용해야 합니다.
-          (princ "\n이 방식은 VLA 메서드를 통해 대화창 없이 블록을 생성합니다.")
-          
-          ;; 실제로는 명령어 대신 아래의 LISP 함수 호출이 더 효과적일 수 있습니다.
-          (command "_.SECTIONPLANETOBLOCK" ent "") 
-          ;; 만약 위 명령어가 대화창을 띄운다면, SendKeys나 별도의 ARX 함수가 필요할 수 있습니다.
-        )
-      )
+      (setq obj (vlax-ename->vla-object sel))
+      ;; GenerateSectionGeometry 메서드 사용 (버전에 따라 다름)
+      (vla-GenerateSectionGeometry obj)
     )
   )
   (princ)
