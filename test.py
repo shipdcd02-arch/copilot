507097a7710d564f3cf03b6e4dc93c6b26011c0f
shipdcd02-arch <shi.pd.cd02@gmail.com>
2026-04-24 11:00:36 +0900

    Upload test.py

--- test.py
+++ test.py
@@ -33,4 +33,40 @@ namespace SectionAutoBlock
             }
         }
     }
+}using System;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+namespace SectionAutoBlock
+{
+    public class Commands
+    {
+        [CommandMethod("DUMPSECTION")]
+        public void DumpSection()
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var ed = doc.Editor;
+
+            var res = ed.GetEntity("\n섹션 플레인 선택: ");
+            if (res.Status != PromptStatus.OK) return;
+
+            using (var tr = doc.Database.TransactionManager.StartTransaction())
+            {
+                var obj = tr.GetObject(res.ObjectId, OpenMode.ForRead);
+                var type = obj.GetType();
+
+                ed.WriteMessage($"\n타입: {type.FullName}");
+                ed.WriteMessage("\n--- 메서드 목록 ---");
+
+                foreach (var method in type.GetMethods())
+                {
+                    ed.WriteMessage($"\n  {method.Name}");
+                }
+
+                tr.Commit();
+            }
+        }
+    }
 }
